c140f07f3c3c450bad1442da72928d1d64f393f9
kyusikkyu <kyusikkyu@gmail.com>
2021-11-10 17:43:32 +0900

    RatioImageView.kt - part 6

---
+++ app/src/main/java/com/rx/ui/customview/RatioImageView.kt
@@ -0,0 +1,19 @@
+package com.rx.ui.customview
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatImageView
+import javax.inject.Inject
+
+class RatioImageView @Inject constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : AppCompatImageView(context, attrs, defStyleAttr) {
+
+    init {
+
+    }
+
+    private fun setupAttributes(context: Context, attrs: AttributeSet?) {
+
+    }
+}
